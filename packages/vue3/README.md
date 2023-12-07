5ce8b8403bd5a2b9ae3321d56e86844d1c0594df
fengyon <fyg1234@yeah.net>
2023-12-07 22:50:19 +0800

    feat(root): init project
    init project

---
+++ packages/vue3/README.md
@@ -0,0 +1,11 @@
+# `vue3`
+
+> TODO: description
+
+## Usage
+
+```
+const vue3 = require('vue3');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/vue3/__tests__/vue3.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const vue3 = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(vue3(), 'Hello from vue3');
+console.info('vue3 tests passed');
